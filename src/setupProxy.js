50958082e0015ff27c6a379fdff71933ce769d61
shreya kondilya <skondilya@gmail.com>
2021-04-06 23:46:53 -0700

    changing the proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const {createProxyMiddleware} = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  	   app.use(createProxyMiddleware('/api', { target: 'http://localhost:4000', changeOrigin: true  }));
+  	   app.use(createProxyMiddleware('/', { target: 'http://localhost:4000', changeOrigin: true  }));
 
 
 };
